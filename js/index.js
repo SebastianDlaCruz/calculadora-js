4b5211805f24e1ac397ff31ec8c503a24205f6ba
sebastian de la cruz <sebastiandelacruz3352gmail.com>
2021-12-28 17:40:35 -0300

    funcionalidades agregadas

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ $simbolosMatematicos = doc.querySelectorAll('#botones button[data-simbolos]');
 
 const pantalla = new Pantalla($primeraPantalla,$segundaPantalla);
 
-//* EVENTOS ///
+//* EVENTOS DE CALCULADORA ///
 
 
 $botonesNumericos.forEach((i)=>{
@@ -33,3 +33,48 @@ $borrar.addEventListener('click', () => pantalla.borrar());
 
  $borrarTodo.addEventListener('click', () => pantalla.borrarTodo());
 
+//* EVENTOS DE CAMBIO DE COLOR // 
+
+const $botones = doc.querySelectorAll('[data-botons]'),
+$container = doc.getElementById('container'),
+$desktop = doc.getElementById('desktop'),
+$igual = doc.getElementById('igual'),
+$dataText = doc.querySelectorAll('[data-texts]'),
+$dataContainers = doc.querySelectorAll('[data-containers]'),
+$dataColors = doc.querySelectorAll('[data-colors="btn"]'),
+$body = doc.getElementById('style');
+
+$botones.forEach((i)=>{
+
+    i.addEventListener('click', (event)=>{
+
+        if(event.target.matches('#color-2')){
+
+            $body.classList.add('style-2');
+            $container.classList.add('style-2');
+            $dataText.forEach(i => i.classList.add('texts-2'));
+            $desktop.classList.add('desktop-2');
+            $dataContainers.forEach(i => i.classList.add('containers-2'));
+            $dataColors.forEach(i => i.classList.add('botones-2'));
+            $igual.classList.add('igual-2');
+        } 
+        
+        else if(event.target.matches('#color-3')){
+
+            $body.classList.add('style-3');
+            /* $container.classList.replace('style-2');
+            $dataText.forEach(i => i.classList.replace('texts-2'));
+            $desktop.classList.replace('desktop-2');
+            $dataContainers.forEach(i => i.classList.replace('containers-2'));
+            $dataColors.forEach(i => i.classList.replace('botones-2'));
+            $igual.classList.replace('igual-2'); */
+
+        }    
+    
+
+
+        
+    
+    });
+
+});
